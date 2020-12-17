510a7d3ae02c3fc78bccd3ae5770d2e95fe047cd
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2020-12-17 15:28:40 +0300

    added running green square

--- js-lesson8/app.js
+++ js-lesson8/app.js
@@ -16,15 +16,73 @@ const h2all = document.querySelectorAll('h2');
 
 console.dir(h2all);
 
-setTimeout(() => {
-    addStylesTo(h1, 'JavaScript');
+const timeOutH1 = setTimeout(() => {
+    addStylesTo(h1, 'JavaScript', '3rem');
 }, 1500);
 
-function addStylesTo(node, text = 'Changed with JS') {
+function addStylesTo(node, text = 'Changed with JS', fontSize) {
     node.textContent = text;
     node.style.color = 'red';
     node.style.textAlign = 'center';
     node.style.background = 'black';
     node.style.padding = '2rem';
+    // false: false, 0, '', null, undefined
+    if (fontSize) {
+        node.style.fontSize = fontSize;
+    }
 }
 
+h1.onclick = (e) => {
+    addStylesTo(e.target, 'You\'ve clicked', '3rem');
+    clearTimeout(timeOutH1);
+}
+
+h1.onmouseover = (ev) => {
+    ev.target.style.background = 'grey';
+}
+
+h1.onmouseleave = (ev) => {
+    ev.target.style.background = 'black';
+}
+
+h1.addEventListener('dblclick', (e) => {
+    e.target.style.color = 'yellow';
+})
+
+const h3 = document.querySelector('h3');
+const link = h3.querySelector('a');
+link.addEventListener('click',(event)=>{
+    event.preventDefault(); // for disabling default behavior of link
+    console.log('Click on link', event.target.getAttribute('href'));
+})
+
+const sq = document.querySelector('#square');
+let x_pos = 200;
+let y_pos = 200;
+setPosition(sq, x_pos, y_pos);
+
+function setPosition(el, x, y) {
+    sq.style.top = y + 'px';
+    sq.style.left = x + 'px';
+}
+
+window.onkeydown = (ev) => {
+    switch (ev.code) {
+        case 'ArrowUp': //up
+            y_pos -= 50;
+            break;
+        case 'ArrowDown': //down
+            y_pos += 50;
+            break;
+        case 'ArrowLeft': //left
+            x_pos -= 50;
+            break;
+        case 'ArrowRight': //right
+            x_pos += 50;
+            break;
+        default:
+            break;
+    }
+    console.log(ev.code);
+    setPosition(sq, x_pos, y_pos);
+}
