5c373d8be349df7d0e6bc3ad030bd17e1745a099
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2020-12-30 13:20:50 +0300

    all about destructuring

---
+++ js-lesson23-destructurization/app.js
@@ -0,0 +1,45 @@
+function calcValues(a, b) {
+    return [
+        a + b,
+        undefined,
+        a * b,
+        a / b
+    ]
+}
+
+const [sum, sub = 'Вычитания нет', ...other] = calcValues(42, 10)
+//const sum = result[0]
+//const sub = result[1]
+// const [sum, sub] = result
+
+console.log(sum, sub, other)
+
+const person = {
+    name: 'Иван',
+    age: 20,
+    address: {
+        country: 'Россия',
+        city: 'Москва'
+    }
+}
+
+const {
+    name: personName,
+    age: personAge,
+    car: personCar = 'нет автомобиля',
+    address: {
+        city: personCity,
+        country: personCountry
+    }
+} = person
+
+const {name: firstName, ...info} = person
+
+console.log(personName, personAge, personCar, personCity, personCountry)
+console.log(firstName, info)
+
+function logPerson({name: firstName, age: personAge}) {
+    console.log(firstName + ' ' + personAge)
+}
+
+logPerson(person)
